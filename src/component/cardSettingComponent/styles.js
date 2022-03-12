aa117cdd93d9a7798f850bae2df72712104329a7
Trushal Rank <91713433+TrushalR@users.noreply.github.com>
2022-03-12 06:20:59 +0530

    Submit assignment

---
+++ src/component/cardSettingComponent/styles.js
@@ -0,0 +1,29 @@
+import colors from '../../common/style/color'
+const styles = {
+    itemTop: {
+        flex: 0.25,
+        alignItems: 'center',
+        flexDirection: 'row',
+    },
+    item: { left: 10, flex: 1 },
+    switch: {
+        height: 10,
+        width: 20,
+        alignItems: 'center',
+        marginBottom: 20,
+        marginRight: 15
+    },
+    switchCard: {
+        height: 10,
+        width: 20,
+        alignItems: 'center',
+        marginBottom: 20, marginLeft: '25%'
+    },
+    icon: {
+        height: 35,
+        width: 35,
+        tintColor: colors.lightBlue
+    },
+    optionDescTextStyle: { color: colors.brown, marginTop: 5 }
+}
+export default styles
